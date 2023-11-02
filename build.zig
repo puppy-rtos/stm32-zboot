45f32a7fbaea4c68e0aa760589a768faf4402fa4
guozhanxin <guozhanxin@rt-thread.com>
2023-11-02 10:09:34 +0800

    f407 can run

--- build.zig
+++ build.zig
@@ -2,9 +2,9 @@ const std = @import("std");
 const stm32 = @import("stm32");
 
 const available_examples = [_]Example{
-    .{ .name = "stm32f103x8", .target = stm32.chips.stm32f103x8, .file = "src/blinky.zig" },
+    // .{ .name = "stm32f103x8", .target = stm32.chips.stm32f103x8, .file = "src/blinky.zig" },
     // TODO: .{ .name = "stm32f303vc", .target = stm32.chips.stm32f303vc, .file = "src/blinky.zig" },
-    // TODO: .{ .name = "stm32f407vg", .target = stm32.chips.stm32f407vg, .file = "src/blinky.zig" },
+    .{ .name = "stm32f407vg", .target = stm32.chips.stm32f407vg, .file = "src/blinky.zig" },
     // TODO: .{ .name = "stm32f429zit6u", .target = stm32.chips.stm32f429zit6u, .file = "src/blinky.zig" },
     // TODO: .{ .name = "stm32f3discovery", .target = stm32.boards.stm32f3discovery, .file = "src/blinky.zig" },
     // TODO: .{ .name = "stm32f4discovery", .target = stm32.boards.stm32f4discovery, .file = "src/blinky.zig" },
@@ -37,6 +37,9 @@ pub fn build(b: *std.Build) void {
 
         // For debugging, we also always install the firmware as an ELF file
         microzig.installFirmware(b, firmware, .{ .format = .elf });
+
+        microzig.installFirmware(b, firmware, .{ .format = .bin });
+        microzig.installFirmware(b, firmware, .{ .format = .hex });
     }
 }
 
--- src/blinky.zig
+++ src/blinky.zig
@@ -1,15 +1,15 @@
 const std = @import("std");
 const microzig = @import("microzig");
-const stm32 = microzig.hal;
+// const stm32 = microzig.hal;
 
-const pin_config = stm32.pins.GlobalConfiguration{
-    .GPIOC = .{
-        .PIN13 = .{ .name = "led", .mode = .{ .output = .general_purpose_push_pull } },
-    },
-};
+// const pin_config = stm32.pins.GlobalConfiguration{
+//     .GPIOC = .{
+//         .PIN13 = .{ .name = "led", .mode = .{ .output = .general_purpose_push_pull } },
+//     },
+// };
 
 pub fn main() !void {
-    const pins = pin_config.apply();
+    // const pins = pin_config.apply();
 
     while (true) {
         var i: u32 = 0;
@@ -17,6 +17,6 @@ pub fn main() !void {
             asm volatile ("nop");
             i += 1;
         }
-        pins.led.toggle();
+        // pins.led.toggle();
     }
 }
