db8683b95360474066d26d29007a2ab1aba3848c
guozhanxin <guozhanxin@rt-thread.com>
2025-05-10 12:23:37 +0800

    Optimize config_addr probe code

--- src/platform/sys/config.zig
+++ src/platform/sys/config.zig
@@ -24,17 +24,22 @@ pub const ZbootConfig = extern struct {
 };
 
 const dconfig = @import("../../default_config.zig");
-var zboot_config: *const ZbootConfig = undefined;
+var zboot_config: *const ZbootConfig = &dconfig.default_config;
 
 // probe config from rom end
 pub fn probe_extconfig(addr: u32) void {
-    const extconfig: *ZbootConfig = @ptrFromInt(addr);
-    if (extconfig.magic != ZBOOT_CONFIG_MAGIC) {
-        zboot_config = &dconfig.default_config;
-        return;
+    var align_addr: u32 = (addr + 3) & ~@as(u32, 3); // Align to 4 bytes
+    const end_addr = addr + 0x1000;
+
+    while (align_addr <= end_addr) {
+        const magic_word: *u32 = @ptrFromInt(align_addr);
+        if (magic_word.* == ZBOOT_CONFIG_MAGIC) {
+            const extconfig: *ZbootConfig = @ptrFromInt(align_addr);
+            zboot_config = extconfig;
+            return;
+        }
+        align_addr += 4;
     }
-    // copy config
-    zboot_config = extconfig;
 }
 
 // get config
